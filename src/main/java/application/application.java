f230d6d255790aa47ddfe327ad50d7b477c265de
NgoHoangLong17 <108849896+NgoHoangLong17@users.noreply.github.com>
2022-11-16 15:40:19 +0700

    Update application.java
    update 333

--- src/main/java/application/application.java
+++ src/main/java/application/application.java
@@ -11,6 +11,6 @@ package application;
 public class application {
 
     public static void main(String[] args) {
-        System.out.println("Xin Chao 222");
+        System.out.println("Xin Chao 333");
     }
 }
