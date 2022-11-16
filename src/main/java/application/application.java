99aaa160076bf2a3104d1c55b47e34e9a0e1a0d8
DESKTOP-Q7QA2Q9\longnh203 <longnhph27503@fpt.edu.vn>
2022-11-16 14:56:22 +0700

    update khung project

---
+++ src/main/java/application/application.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package application;
+
+/**
+ *
+ * @author longnh203
+ */
+public class application {
+
+    public static void main(String[] args) {
+        System.out.println("Xin Chao");
+    }
+}
