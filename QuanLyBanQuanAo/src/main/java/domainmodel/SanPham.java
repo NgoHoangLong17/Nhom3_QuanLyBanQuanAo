5ce4724a0849618f38a37e257bc40f3a14b41c90
thienddph27448 <thienddph27448@fpt.edu.vn>
2022-11-20 22:39:25 +0700

    sanpham

---
+++ QuanLyBanQuanAo/src/main/java/domainmodel/SanPham.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domainmodel;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author doand
+ */
+@Entity
+public class SanPham {
+ private int id ;
+ private String ten;
+}
