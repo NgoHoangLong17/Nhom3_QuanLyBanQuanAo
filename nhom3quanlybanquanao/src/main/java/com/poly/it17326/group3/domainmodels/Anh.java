964b76e01cff50c62951a068eba41a9de84c0d3a
sonphph27515 <sonphph27515@fpt.edu.vn>
2022-11-21 15:39:14 +0700

    anh

--- nhom3quanlybanquanao/src/main/java/com/poly/it17326/group3/domainmodels/Anh.java
+++ nhom3quanlybanquanao/src/main/java/com/poly/it17326/group3/domainmodels/Anh.java
@@ -4,10 +4,35 @@
  */
 package com.poly.it17326.group3.domainmodels;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  *
  * @author ADMIN
  */
+@Entity
+@Table(name = "ANH")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Anh {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
     
+    @Column(name = "TEN")
+    private String ten;
 }
