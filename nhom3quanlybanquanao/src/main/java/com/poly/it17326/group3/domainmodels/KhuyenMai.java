5a345d1bfe7239be84afe1bc842feb847b8c8152
thienddph27448 <thienddph27448@fpt.edu.vn>
2022-11-23 08:43:31 +0700

    thu gio hang

--- nhom3quanlybanquanao/src/main/java/com/poly/it17326/group3/domainmodels/KhuyenMai.java
+++ nhom3quanlybanquanao/src/main/java/com/poly/it17326/group3/domainmodels/KhuyenMai.java
@@ -1,49 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.poly.it17326.group3.domainmodels;
-
-import java.util.Date;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-/**
- *
- * @author longnh203
- */
-@Entity
-@Table(name = "KHUYENMAI")
-@Setter
-@Getter
-@ToString
-@AllArgsConstructor
-@NoArgsConstructor
-public class KhuyenMai {
-
-    @Id
-    @Column(name = "ID")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
-    @Column(name = "TEN")
-    private String ten;
-
-    @Column(name = "MUCGIAMGIA")
-    private int mucGiamGia;
-
-    @Column(name = "NGAYBATDAU")
-    private Date ngayBatDau;
-
-    @Column(name = "NGAYKETTHUC")
-    private Date ngayKetThuc;
-}
+//git/*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+// */
+//package com.poly.it17326.group3.domainmodels;
+//
+//import java.util.Date;
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import lombok.ToString;
+//
+///**
+// *
+// * @author longnh203
+// */
+//@Entity
+////@Table(name = "KHUYENMAI")
+////@Setter
+////@Getter
+////@ToString
+////@AllArgsConstructor
+////@NoArgsConstructor
+//public class KhuyenMai {
+//
+////    @Id
+////    @Column(name = "ID")
+////    @GeneratedValue(strategy = GenerationType.IDENTITY)
+////    private int id;
+////
+////    @Column(name = "TEN")
+////    private String ten;
+////
+////    @Column(name = "MUCGIAMGIA")
+////    private int mucGiamGia;
+////
+////    @Column(name = "NGAYBATDAU")
+////    private Date ngayBatDau;
+////
+////    @Column(name = "NGAYKETTHUC")
+////    private Date ngayKetThuc;
+//}
