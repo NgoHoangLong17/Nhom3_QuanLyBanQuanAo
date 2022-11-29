a95bb1a86a84ee9dd40d81fb653f38a93f74a91d
DESKTOP-Q7QA2Q9\longnh203 <longnhph27503@fpt.edu.vn>
2022-11-29 09:06:19 +0700

    fix lỗi

--- nhom3quanlybanquanao/src/main/java/com/poly/it17326/group3/view/mainJFrame.java
+++ nhom3quanlybanquanao/src/main/java/com/poly/it17326/group3/view/mainJFrame.java
@@ -324,6 +324,8 @@ public class MainJFrame extends javax.swing.JFrame {
         }
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
